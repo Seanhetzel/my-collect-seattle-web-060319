baf0262b99389b4806c22566472b122c13b11c3f
Sean Hetzel <seanhetzel1@gmail.com>
2019-05-18 02:28:21 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(array)
+  moded_array = []
+  i = 0 
+  while i < array.length 
+    #yield array[i]
+    moded_array.push(yield array[i])
+    i += 1 
+  end
+  moded_array
+end
